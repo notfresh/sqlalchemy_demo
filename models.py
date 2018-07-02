d6d5733a647d6b1c221fe3da2dc05e183047aba0
zhengxu <1195056983@qq.com>
2018-07-02 23:08:36 +0800

    一些普通查询

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, DECIMAL, ForeignKey
 
 from db_util import Base, Session
 
@@ -16,4 +16,24 @@ class Dept(Base):
             'deptno': self.deptno,
             'dname': self.dname,
             'loc': self.loc
+        })
+
+
+class Emp(Base):
+    __tablename__ = 'emp'
+    empno = Column(Integer, primary_key=True)
+    ename = Column(String(10))
+    job = Column(String(9))
+    mgr = Column(Integer)
+    hiredate = Column(Date)
+    comm = Column(DECIMAL(7, 2))
+    deptno = Column(Integer, ForeignKey('dept.deptno'))
+
+    def __repr__(self):
+        return str({
+            'empno': self.empno,
+            'ename': self.ename,
+            'job': self.job,
+            'deptno': self.deptno,
+            'comm': self.comm
         })
---
+++ query_demo.py
@@ -0,0 +1,35 @@
+from models import *
+
+
+def query_emp():
+    emp1 = sess.query(Emp).first()
+    print(emp1)
+
+
+# 查询名字叫Smith的员工
+def query_emp_with_filter():
+    emp1 = sess.query(Emp).filter(Emp.ename == 'Smith').first()
+    print(emp1)
+
+
+# 查询名字叫Smith, 职务为CLERK的一个员工
+def query_emp_with_filters():
+    emp1 = sess.query(Emp).filter(Emp.ename == 'Smith', Emp.job == 'CLERK').first()
+    print(emp1)
+
+
+# 查询职务为CLERK的全部员工
+def query_emp_all_clerks():
+    emp_clerks = sess.query(Emp).filter(Emp.job == 'CLERK').all()
+    for clerk in emp_clerks:
+        print(clerk)
+
+
+# 查询员工的名字和工号, 并按入职日期排序
+def query_emp_empno_ename_order_by_hiredate():
+    emps = sess.query(Emp.empno, Emp.ename, Emp.job, Emp.hiredate).order_by(Emp.hiredate.asc()).all()
+    for item in emps:
+        print(item.empno, item.ename, item.job, item.hiredate)
+
+if __name__ == '__main__':
+    query_emp_empno_ename_order_by_hiredate()
